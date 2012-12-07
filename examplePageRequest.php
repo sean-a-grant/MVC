aa0aad649b9720225013ce0a77fd0d2aeb575e14
Sean Grant <sean.grant@ymail.com>
2012-12-06 16:27:28 -0800

    Added hello world file example, commented GET example

--- examplePageRequest.php
+++ examplePageRequest.php
@@ -3,7 +3,8 @@
 include 'c/index.php';
 new page('helloworld');
 
-// If you want to use $_GET (Look into .htaccess for more functionality)
-$page = (isset($_GET['__page'])) ? $_GET['__page'] : 'default';
-new page($page);
+// If you want to use $_GET (Look into .htaccess for more functionality):
+//
+// $page = (isset($_GET['__page'])) ? $_GET['__page'] : 'default';
+// new page($page);
 ?>
