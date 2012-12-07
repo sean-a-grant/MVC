a5d5ee4b7d09c50bf8e56929340395fe6893ea37
Sean Grant <sean.grant@ymail.com>
2012-12-06 16:25:41 -0800

    Initial commit

---
+++ examplePageRequest.php
@@ -0,0 +1,9 @@
+<?php
+// Static
+include 'c/index.php';
+new page('helloworld');
+
+// If you want to use $_GET (Look into .htaccess for more functionality)
+$page = (isset($_GET['__page'])) ? $_GET['__page'] : 'default';
+new page($page);
+?>
